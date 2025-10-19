82fe779160d14c88e4a3d0cc724b6fc5419a024e
Davron Aglayev <aglayev999@gmail.com>
2025-10-19 05:57:36 +0000

    qara biziki iphone ga uxshamaydi juda kichik va tortburchal va blur back

--- src/contexts/AlertDialogContext.tsx
+++ src/contexts/AlertDialogContext.tsx
@@ -10,7 +10,6 @@ import {
   AlertDialogHeader,
   AlertDialogTitle,
 } from "@/components/ui/alert-dialog";
-import { Button } from '@/components/ui/button';
 
 interface AlertOptions {
   title: React.ReactNode;
